87dd35239a5214936ec8984f7a7980677c699402
Kateryna Dementieva <katerynadementieva@gmail.com>
2024-03-26 00:40:03 +0100

    Added email verification

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 
 const apiUrl = 'http://localhost:5193';
-const checkAuth = () => {
-  let x = document.cookie;
-  if (!x.includes("autenticated=true"))
-  {
-    window.location.href = "login.html";
-  }
-}
 
 const loginUser = () => {
   let userName = document.getElementById("login").value;
@@ -21,32 +14,115 @@ const loginUser = () => {
     headers: {
       "Content-Type": "application/json"
     },
-    credentials: 'include',
     body: JSON.stringify(data),
   };
 
   fetch(`${apiUrl}/auth`, requestOptions)
-    .then(response =>{
+    .then(response => {
       if (!response.ok) {
         if (response.status === 401) {
           alert("Niepoprawny login lub hasło!")
         } else {
           alert ("Nieznany błąd!")
         }
-      } else {
-        // window.location.href = "index.html";
-        console.log(response)
+      }
+      else {
+        response.text().then(data =>
+          document.cookie = `auth_token=${data}`
+        )
+        window.location.href = "index.html";
       }
     });
 }
 
-const logRequire = () => {
-  let x = document.cookie;
-  if (x.includes("autenticated=true"))
-  {
-    window.location.href = "index.html";
-  }
 
+
+const UserData = () => {
+  var cookies = document.cookie.split("; ");
+  var token = cookies.find(i => i.startsWith("auth_token=")).split("=")[1]
+
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  };
+
+  fetch(`${apiUrl}/client`, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      window.location.href = "login.html";
+    }
+    else {
+      response.json().then(data =>
+        document.getElementById('data').innerHTML = JSON.stringify(data)
+      )
+    }
+  })
 }
 
 
+
+const CreateClient = () => {
+  let userName = document.getElementById("FirstName").value;
+  let userSecondName = document.getElementById("SecondName").value;
+  let userLastName = document.getElementById("LastName").value;
+  let userDateOfBirth = document.getElementById("DateOfBirth").value;
+  let userLogin = document.getElementById("login").value;
+  let userPassword = document.getElementById("password").value;
+
+  const data ={
+    FirstName: userName,
+    SecondName: userSecondName,
+    LastName: userLastName,
+    DateOfBirth: userDateOfBirth,
+    CLogin: userLogin,
+    CPassword: userPassword,
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      "Content-Type": `application/json`
+    },
+    body: JSON.stringify(data),
+  };
+
+  fetch(`${apiUrl}/client`, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      alert ("Błąd!")
+    }
+    else {
+      alert ("Wysłano email!")
+      window.location.href = 'code.html'
+    }
+  })
+}
+
+const VerifyCod = () => {
+  let UserCode = document.getElementById("code").value;
+
+const data ={
+  verifyCod: UserCode,
+};
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    "Content-Type": `application/json`
+  },
+  body: JSON.stringify(data),
+};
+
+  fetch(`${apiUrl}/client`, requestOptions)
+  .then(response => {
+    if (!response.ok) {
+      alert ("Błąd!")
+    }
+    else {
+      alert ("Poprawny kod!")
+      window.location.href = 'login.html'
+    }
+  })
+}
